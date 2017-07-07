77f3f3a69499763a1a2e2ab4fe3db8840efb39c7
LuiRa <luisa.rademacher@jow.de>
2017-07-07 14:34:32 +0200

    added tests for arrays

--- about/arrays.rb
+++ about/arrays.rb
@@ -23,6 +23,40 @@ class ArrayPlayground < Minitest::Test
     assert_equal [2, 4, 6], a.map { |n| n * 2 }
 
   end
+  
+  def test_create_empty_array
+	a = Array.new(5)
+	assert_equal 5, a.length
+  end
+  
+  def test_add_item_to_end
+	a = ["a", "b", "c"]
+	a << "d"
+	assert_equal "d", a[-1]
+  end
+  
+  def test_destructive_selection
+	a = ["We", "Say", "Hello", "To", "The", "World"]
+	a.keep_if { |str| (str.include? "H") or (str.include? "Wo")}
+	assert_equal ["Hello", "World"], a
+  end
+  
+  def test_intersection
+	a = [1, 3, 5, 1] & [1, 1, 2, 3, 4]    
+	assert_equal [1, 3], a
+  end
+  
+  def test_create_string_from_array 
+	str = [ 1, 2, 3 ] * ","  
+	assert_equal "1,2,3", str
+  end
+  
+  def test_concatenate_arrays
+	a1 = ["Media Programming", "2017", "Web Technology"]
+	a2 = ["Ruby-Learning", "Rails-Tutorial"]
+	a3 = a1 + a2
+	assert_equal ["Media Programming", "2017", "Web Technology", "Ruby-Learning", "Rails-Tutorial"], a3
+  end
 
 
 end
